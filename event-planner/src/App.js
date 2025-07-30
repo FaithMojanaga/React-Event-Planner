b4197c55d5cd41646d238a8f33e0c4b9b84a5cef
Faith Konopo <fcse22-011@thuto.bac.ac.bw>
2025-07-30 15:16:13 +0200

    added comments

--- event-planner/src/App.js
+++ event-planner/src/App.js
@@ -1,5 +1,7 @@
+// Import the EventPlanner component to be rendered within the App
 import EventPlanner from './components/EventPlanner';
 
+// Main App component renders the EventPlanner inside a container div
 function App() {
   return (
     <div className="App">
